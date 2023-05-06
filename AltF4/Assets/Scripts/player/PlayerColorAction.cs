12e48dc523c53050cc0324b98087a53aa02b1db5
Michael <51726968+MicSilvaSL@users.noreply.github.com>
2023-05-06 14:34:16 -0300

    a

--- AltF4/Assets/Scripts/player/PlayerColorAction.cs
+++ AltF4/Assets/Scripts/player/PlayerColorAction.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PlayerColorAction : MonoBehaviour
 {
-
+    private PlayerControl input;
+    private IColor currentColor;
 
     void Update()
     {
+        if (input.ColorButton)
+        {
+            currentColor?.Action();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
         
     }
-    void ColorAction()
-    {
-
-    }
+    
 }
