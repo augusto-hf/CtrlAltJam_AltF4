70ca853850bd67098729b563c8009c75cd1ca0e9
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-05-06 16:46:13 -0300

    blueaction

--- AltF4/Assets/Scripts/ColorActionInterface/BlueAction.cs
+++ AltF4/Assets/Scripts/ColorActionInterface/BlueAction.cs
@@ -6,10 +6,11 @@ public class BlueAction : MonoBehaviour, IColor
 {
     [SerializeField] private PlayerData dataScript;
     [SerializeField] private PlayerControl inputScript;
-    [SerializeField] private PlayerMoviment moveScript;
-    [SerializeField] private PlayerChecks checkScript;
     public void Action(GameObject player)
     {
+        PlayerMoviment moveScript = player.GetComponent<PlayerMoviment>();
+        PlayerChecks checkScript = player.GetComponent<PlayerChecks>();
+
         if (checkScript.IsGrounded)
             player.GetComponent<Rigidbody2D>().velocity = new Vector2(player.GetComponent<Rigidbody2D>().velocity.x, dataScript.JumpForce);
     }
