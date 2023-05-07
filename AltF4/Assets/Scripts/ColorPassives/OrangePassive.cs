45d943bc9a527a2d97d97836af34e5a979b019ac
Michael <51726968+MicSilvaSL@users.noreply.github.com>
2023-05-07 01:38:05 -0300

    inicial block passive

---
+++ AltF4/Assets/Scripts/ColorPassives/OrangePassive.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrangePassive : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var movement = other.gameObject.GetComponent<PlayerMovement>();
+            RunPassive(movement);
+        }   
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        var movement = other.gameObject.GetComponent<PlayerMovement>();
+        RunPassive(movement);
+    }
+
+
+    public void RunPassive(PlayerMovement movement)
+    {
+        movement.SetMaxSpeed(movement.Data.MaxRunSpeed);
+    }
+
+    public void DisablePassive(PlayerMovement movement)
+    {
+        movement.SetMaxSpeed(movement.Data.MaxHorizontalSpeed);
+    }
+
+    
+}
