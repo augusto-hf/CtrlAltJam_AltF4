191465a450ab4eb60d86d91405e606a2c72bd896
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-05-07 17:55:47 -0300

    Hook

--- AltF4/Assets/Scripts/player/PlayerControl.cs
+++ AltF4/Assets/Scripts/player/PlayerControl.cs
@@ -6,11 +6,13 @@ public class PlayerControl : MonoBehaviour
 {
     public Vector2 Axis { get => GetAxis(); }
     public bool ColorButton { get; private set; }
+    public bool TongueButton { get; private set; }
 
     // Update is called once per frame
     void Update()
     {
         ColorButton = Input.GetButton("ColorActionButton");
+        TongueButton = Input.GetButton("Tongue");
     }
 
     private Vector2 GetAxis()
--- AltF4/Assets/Scripts/player/PlayerHook.cs
+++ AltF4/Assets/Scripts/player/PlayerHook.cs
@@ -11,8 +11,8 @@ public class PlayerHook : MonoBehaviour
     [SerializeField] private float hookSpeed, hookMaxDistance;
     [SerializeField] private LayerMask grapplableMask;
 
-    private Vector2 target;
-    private bool isGrappling = false, returningGrapple = false;
+    private GameObject targetObject;
+    private bool isGrappling = false, retractingGrapple = false, startedGrappling = false, finishedGrappling = false;
 
     void Awake()
     {
@@ -21,11 +21,23 @@ public class PlayerHook : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (player.Input.TongueButton && !retractingGrapple)
         {
             StartHook();
         }
-        
+
+        if (retractingGrapple)
+        {
+            line.SetPosition(0, transform.position);
+            if (!player.Input.TongueButton && !finishedGrappling)
+            {
+                finishedGrappling = true;
+                StartCoroutine(returnGrapple());               
+            }
+
+            
+            if (targetObject.CompareTag("Grappable") && !finishedGrappling)
+        }
     }
     public void StartHook()
     {
@@ -37,31 +49,54 @@ public class PlayerHook : MonoBehaviour
             return;
  
         isGrappling = true;
-        target = hit.transform.position;
+        targetObject = hit.transform.gameObject;
         line.enabled = true;
         line.positionCount = 2;
 
-        StartCoroutine(Grapple());
+        if(!startedGrappling)
+            StartCoroutine(Grapple());
     }
-
     IEnumerator Grapple()
     {
+        Debug.Log("Joguei");
         float time = 10;
-        Vector2 grapplingEnd = transform.position;
+        startedGrappling = true;
 
         line.SetPosition(0, transform.position);
-        line.SetPosition(1, grapplingEnd);
-
+        Vector2 grapplingEnd = transform.position;
 
         for (float t = 0; t < time; t += hookSpeed * Time.deltaTime)
         {
-            grapplingEnd = Vector2.Lerp(transform.position, target, t / time);
+            grapplingEnd = Vector2.Lerp(transform.position, targetObject.transform.position, t / time);
             line.SetPosition(0, transform.position);
             line.SetPosition(1, grapplingEnd);
             yield return null;
         }
+        startedGrappling = false;
+        retractingGrapple = true;
+    }
+    IEnumerator returnGrapple()
+    {
+        Debug.Log("Voltou");
+        float time = 10;
 
-        line.SetPosition(1, target);
-        returningGrapple = true;
+        Vector2 grapplingEnd = targetObject.transform.position;
+
+        for (float t = 0; t < time; t += hookSpeed * Time.deltaTime)
+        {
+            grapplingEnd = Vector2.Lerp(targetObject.transform.position, transform.position, t / time);
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, grapplingEnd);
+            
+            yield return null;
+        }
+        if (Vector2.Distance(transform.position, grapplingEnd) < 0.5f)
+        {
+            retractingGrapple = false;
+            isGrappling = false;
+            line.enabled = false;
+            finishedGrappling = false;
+        }
+
     }
 }
