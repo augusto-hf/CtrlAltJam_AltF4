061224345a6b20c33f9334178bec0d443a862f52
Evandro França <62087611+Vandynn@users.noreply.github.com>
2023-05-06 23:46:31 -0300

    camera + menu

---
+++ AltF4/Assets/Scripts/Menu/CameraMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private  float smoothTime = 0.3f; 
+
+    [SerializeField] private bool menuActive = false;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuActive = !menuActive;
+            GetTarget();
+        }
+    }
+
+    void GetTarget()
+    {
+        if(!menuActive)
+        {
+            target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            return;
+        }
+
+        target = GameObject.FindWithTag("Menu").GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        if(target != null )
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z); 
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
