740aeb247fede8c4c3cda8700c35d5c6c21955e5
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-05-07 14:19:49 -0300

    Starting hook

---
+++ AltF4/Assets/Scripts/player/PlayerHook.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerHook : MonoBehaviour
+{
+    private PlayerCore player;
+
+    [SerializeField] private Transform hookStarterPoint, hookEndPoint;
+    [SerializeField] private float hookSpeed, hookMaxDistance, maxDistance;
+    [SerializeField] private LayerMask grapplableMask;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        player = GetComponent<PlayerCore>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            StartHook();
+        }
+    }
+    public void StartHook()
+    {
+        Vector2 direction = transform.forward;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, maxDistance, grapplableMask);
+
+    }
+}
