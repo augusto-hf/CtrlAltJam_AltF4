647db6cb7eef6be699e243d80ee3b4f45f230bb5
Michael <51726968+MicSilvaSL@users.noreply.github.com>
2023-05-09 23:12:20 -0300

    Update menu camera

--- AltF4/Assets/Scripts/Camera/CameraMove.cs
+++ AltF4/Assets/Scripts/Camera/CameraMove.cs
@@ -5,24 +5,19 @@ using System;
 
 public class CameraMove : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-    [SerializeField] private  float smoothTimeMenu = 0.6f; 
-
-    [SerializeField] private  float smoothTimeGame = 0.1f; 
-   
-    [SerializeField] private  float offSetXPlayer; 
-    [SerializeField] private  float offSetYPlayer; 
-
+    
     [SerializeField] private bool menuActive = false;
     
-
     private float smoothTimeFinal = 0.6f;
     private float offSetXFinal; 
     private float offSetYFinal;
 
     private Vector3 velocity = Vector3.zero;
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private GameObject canvasCamera;
 
     public event Action openMenu;
+
 
     private void Update() 
     {
@@ -32,38 +27,23 @@ public class CameraMove : MonoBehaviour
         }
     }
 
-    void LateUpdate()
-    {
-        if(target != null )
-        {
-            Vector3 targetPosition = new Vector3(target.position.x  + offSetXFinal, target.position.y + offSetYFinal, transform.position.z); 
-
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTimeFinal);
-        }
-    }
-
     public void ChangeTarget()
     {
         menuActive = !menuActive;
 
         if(!menuActive)
         {
-            target = GameObject.FindWithTag("Player").GetComponent<Transform>();
-            StartCoroutine(SetOffSetFinal(offSetXPlayer, offSetYPlayer, smoothTimeGame));
+            playerCamera.SetActive(false);
+            canvasCamera.SetActive(true);
             return;
         }
 
-        target = GameObject.FindWithTag("Menu").GetComponent<Transform>();
-        StartCoroutine(SetOffSetFinal(0, 0, smoothTimeMenu));
+        playerCamera.SetActive(true);
+        canvasCamera.SetActive(false);
+
         openMenu?.Invoke();
     }
 
 
-    IEnumerator SetOffSetFinal(float x, float y, float smooth)
-    {
-        offSetXFinal = x;
-        offSetYFinal = y;
-        yield return new WaitForSeconds(0.2f);
-        smoothTimeFinal = smooth;
-    }
+    
 }
