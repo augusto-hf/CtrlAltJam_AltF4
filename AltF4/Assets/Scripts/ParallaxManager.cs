687916291b0266bfa174bf84abf519097930fada
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-05-13 17:56:18 -0300

    Update ParallaxManager.cs

--- AltF4/Assets/Scripts/ParallaxManager.cs
+++ AltF4/Assets/Scripts/ParallaxManager.cs
@@ -6,19 +6,26 @@ public class ParallaxManager : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private Transform ImageToParallax;
-    Vector2 StartPosition;
-    float StartZ;
+    Vector2 startPosition;
+    float startZ;
 
-    Vector2 travel =>  (Vector2)cam.transform.position - StartPosition;
+    Vector2 travel =>  (Vector2)cam.transform.position - startPosition;
+
+    float distanceFromSubject => transform.position.z - ImageToParallax.position.z;
+
+    float clippingPlane => cam.transform.position.z + (distanceFromSubject > 0? cam.farClipPlane : cam.nearClipPlane);
+
+    float parallaxFactor => Mathf.Abs(distanceFromSubject/clippingPlane);
     void Start()
     {
-        StartPosition = transform.position;
-        StartZ = transform.position.z;
+        startPosition = transform.position;
+        startZ = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 newPos = startPosition + travel * parallaxFactor;
+        transform.position = new Vector3(newPos.x, newPos.y, startZ)
     }
 }
