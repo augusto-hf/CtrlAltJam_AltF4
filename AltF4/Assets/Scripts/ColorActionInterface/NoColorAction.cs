397029377f3c32541b907166eee9b46cb8a01ff8
Michael <51726968+MicSilvaSL@users.noreply.github.com>
2023-05-24 19:26:55 -0300

    blob update

--- AltF4/Assets/Scripts/ColorActionInterface/NoColorAction.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NoColorAction : MonoBehaviour, IColor
-{
-    [SerializeField] private ColorData _colorData;
-    public ColorData ColorData { get => _colorData; }
-}
--- AltF4/Assets/Scripts/ColorActionInterface/PurpleAction.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PurpleAction : MonoBehaviour, IColor
-{
-    public ColorData ColorData { get; private set; }
-
-}
