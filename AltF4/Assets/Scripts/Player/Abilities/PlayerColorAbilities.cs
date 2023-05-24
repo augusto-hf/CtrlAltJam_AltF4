0f59fa3526874e70c6f8e73fc633a61984ff8cff
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-05-24 18:11:57 -0300

    Manger

--- AltF4/Assets/Scripts/Player/Abilities/PlayerColorAbilities.cs
+++ AltF4/Assets/Scripts/Player/Abilities/PlayerColorAbilities.cs
@@ -104,12 +104,12 @@ public class PlayerColorAbilities : MonoBehaviour
         else if (player.Controller.ColorButtonUp && player.Movement.Velocity.y > 0)
         {
             JumpCutForceApply();
-            player.Colormanager.ConsumeColor();
+            player.ColorManager.ConsumeColor();
         }
         else if (player.Check.OnGround() && isJumping && jumpCharge <= 0 & player.Movement.Velocity.y < 0)
         {
             isJumping = false;
-            player.Colormanager.ConsumeColor();
+            player.ColorManager.ConsumeColor();
         }
         
     }
