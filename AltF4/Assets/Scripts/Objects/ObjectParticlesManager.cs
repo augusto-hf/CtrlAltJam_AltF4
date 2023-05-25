6a8c2f98646a454f56b0a2e245675e8d195d626a
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-05-24 22:51:23 -0300

    Update ObjectParticlesManager.cs

--- AltF4/Assets/Scripts/Objects/ObjectParticlesManager.cs
+++ AltF4/Assets/Scripts/Objects/ObjectParticlesManager.cs
@@ -1,31 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Obsolete]
 public class ObjectParticlesManager : MonoBehaviour
 {
     [Header("Particle Variables")]
-    [SerializeField] private ParticleSystem landingParticles, dragParticles;
+    [SerializeField] private ParticleSystem landingParticle, dragRightParticle, dragLeftParticle;
     [SerializeField] private Transform downPoint, downRightPoint, downLeftPoint;
     [Header ("Ground Detector Variables")]
     [SerializeField] private LayerMask solid;
     [SerializeField] private LayerMask ground;
     [SerializeField] private Transform groundDetectorPoint;
     [SerializeField] private Vector2 size;
+    private bool alreadyTouchedGround = false;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        
+        particleOnLanding();
+        particleOnDrag();
     }
     void particleOnLanding()
     {
-
+        if (OnGround() && !alreadyTouchedGround)
+        {
+            alreadyTouchedGround = true;
+            playOneTimeParticle(landingParticle, downPoint.position);
+        }
+        if (!OnGround())
+        {
+            alreadyTouchedGround = false;
+        }
     }
+
     void particleOnDrag()
     {
-
+        if (OnGround() && rb.velocity.x > 0.5f)
+        {
+            stopConstantParticle(dragLeftParticle);
+            playConstantParticle(dragRightParticle);
+        }
+        else if (OnGround() && rb.velocity.x < -0.5f)
+        {
+            stopConstantParticle(dragRightParticle);
+            playConstantParticle(dragLeftParticle);
+        }
+        else
+        {
+            stopConstantParticle(dragLeftParticle);
+            stopConstantParticle(dragRightParticle);
+        }
     }
-    public bool OnGround()
+    private bool OnGround()
     {
         var groundCheck = Physics2D.OverlapBox(groundDetectorPoint.position, size, 0, solid);
         return groundCheck;
