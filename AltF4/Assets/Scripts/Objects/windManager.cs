3d5c9c2aa2e4919e3c5d7278220d70f09757154a
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-06-17 10:46:40 -0300

    Update
    -Wind manager is working as intended
    -Adjusting tongue width

--- AltF4/Assets/Scripts/Objects/windManager.cs
+++ AltF4/Assets/Scripts/Objects/windManager.cs
@@ -7,28 +7,46 @@ public class windManager : MonoBehaviour
     private AreaEffector2D effector;
     private Collider2D collider;
     [SerializeField] private bool iAmHorizontal;
-    [SerializeField] private float horizontalRunnableForceMagnitude;
-    private float idealForceMagnitude;
+    [SerializeField] private float runnableForceMagnitude, runnableDrag;
+    private float originalForceMagnitude, originalDrag;
+    private PlayerCore player;
 
     private void Start()
     {
         collider = GetComponent<Collider2D>();
         effector = GetComponent<AreaEffector2D>();
-        idealForceMagnitude = effector.forceMagnitude;
+        originalForceMagnitude = effector.forceMagnitude;
+        originalDrag = effector.drag;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && iAmHorizontal && collision.GetComponent<PlayerCore>().ColorManager.CurrentColor.ColorData.Type == ColorType.Orange)
+        if (collision.gameObject.CompareTag("Player"))
+            player = collision.GetComponent<PlayerCore>();
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && iAmHorizontal)
         {
-            effector.forceMagnitude = horizontalRunnableForceMagnitude;
+            if (player.ColorManager.CurrentColor.ColorData.Type == ColorType.Orange && player.Controller.ColorButtonHold && player.Controller.Axis.x != 0) 
+            {                
+                effector.forceMagnitude = runnableForceMagnitude;
+                effector.drag = runnableDrag;
+            }
+            else
+            {
+                effector.forceMagnitude = originalForceMagnitude;
+                effector.drag = originalDrag;
+            }
         }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && iAmHorizontal && collision.GetComponent<PlayerCore>().ColorManager.CurrentColor.ColorData.Type == ColorType.Orange)
+        if (collision.gameObject.CompareTag("Player") && iAmHorizontal)
         {
-            effector.forceMagnitude = idealForceMagnitude;
+            effector.forceMagnitude = originalForceMagnitude;
+            effector.drag = originalDrag;
         }
     }
 }
