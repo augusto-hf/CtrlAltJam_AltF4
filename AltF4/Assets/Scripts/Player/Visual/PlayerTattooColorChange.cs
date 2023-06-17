38bb6ccfc09b189b178d1bb67d41f2c072d89ea5
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-06-17 12:06:35 -0300

    update

--- AltF4/Assets/Scripts/Player/Visual/PlayerTattooColorChange.cs
+++ AltF4/Assets/Scripts/Player/Visual/PlayerTattooColorChange.cs
@@ -12,6 +12,7 @@ public class PlayerTattooColorChange : MonoBehaviour
     [SerializeField] private GameObject noColorReference;
     [SerializeField] private Color blue, orange, noColor;
     [SerializeField] private float transitionTime;
+    private bool isInstantColor;
     void Start()
     {
         lastColorPower = noColorReference.gameObject.GetComponent<IColor>().ColorData.Type;
@@ -28,29 +29,37 @@ public class PlayerTattooColorChange : MonoBehaviour
         if (currentColor != nextColor)
         {
             //playerSprite.material.color = currentColor = nextColor;
-            StartCoroutine(changeColor());           
+            if (isInstantColor)
+                StartCoroutine(changeInstantColor());
+            else
+                changeRelativeColor();
         }
         
     }
     private void identifyColorChange()
     {
-        if (nextColorPower == ColorType.Blue)
+        switch (nextColorPower)
         {
-            lastColor = currentColor;
-            nextColor = blue;
-        }
-        else if (nextColorPower == ColorType.Orange)
-        {
-            lastColor = currentColor;
-            nextColor = orange;
-        }
-        else if (nextColorPower == ColorType.NoColor)
-        {
-            lastColor = currentColor;
-            nextColor = noColor;
+            case ColorType.Blue:
+                lastColor = currentColor;
+                nextColor = blue;
+                isInstantColor = true;
+                break;
+
+            case ColorType.Orange:
+                lastColor = currentColor;
+                nextColor = orange;
+                isInstantColor = false;
+                break;
+
+            case ColorType.NoColor:
+                lastColor = currentColor;
+                nextColor = noColor;
+                isInstantColor = true;
+                break;
         }
     }
-    private IEnumerator changeColor()
+    private IEnumerator changeInstantColor()
     {
         float percentage = 0;
         while (currentColor != nextColor)
@@ -60,5 +69,10 @@ public class PlayerTattooColorChange : MonoBehaviour
             yield return null;
         }
         lastColor = currentColor;
+    }
+    private void changeRelativeColor()
+    {
+        Debug.Log(currentColor);
+        playerSprite.material.color = currentColor = Color.Lerp(lastColor, nextColor, (player.Abilities.StaminaAmount / PlayerStamina.MAX_STAMINA) * Time.deltaTime);
     }
 }
