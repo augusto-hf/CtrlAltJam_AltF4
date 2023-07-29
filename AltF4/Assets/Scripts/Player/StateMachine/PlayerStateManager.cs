b84a69b506753584299198027da0de52ac49b6fc
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-07-28 21:21:47 -0300

    Update
    starting state machine

---
+++ AltF4/Assets/Scripts/Player/StateMachine/PlayerStateManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateManager : MonoBehaviour
+{
+
+    PlayerBaseState currentState;
+    PlayerWalkingState walkingState = new PlayerWalkingState();
+    PlayerTongueState tongueState = new PlayerTongueState();
+    PlayerFallingState fallingState = new PlayerFallingState();
+    PlayerPowerJumpingState powerJumpingState = new PlayerPowerJumpingState();
+    PlayerPowerRunningState runningState = new PlayerPowerRunningState();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
